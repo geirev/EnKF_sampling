7a04073414fc87b6c7eedb4d906a94276999a3dc
geirev <geir.evensen@gmail.com>
2019-10-26 16:32:18 +0200

    sample.F90 for generating ERT-eclipse rate perturbations The E0pert.dat file is read by schedule_parcer to perturb history rates

--- test/sample.F90
+++ test/sample.F90
@@ -23,7 +23,7 @@ program sample
    character(len=100) :: obshistfile='obshistnew.txt' 
    character(len=100) :: schedulefile='history.sch' 
    integer n1                             ! x-dimension of grid
-   integer i,j,k,ic
+   integer i,j,k,l,m,ic
    integer idat
    integer iwell
    integer ipos(4)
@@ -31,10 +31,9 @@ program sample
 
    real B(nx,nrwells*nrdata*nrens)                       !  1D samples
    real A(nx*nrwells*nrdata,nrens)
-   real C(nx,nrwells*nrdata,nrens)
-   real obs(nx,nrwells,nrdata)
-   real obs1(nx,nrwells*nrdata)
-   real stddev(nx,nrwells*nrdata)
+   real C(nx, nrdata, nrwells, nrens)
+   real obs(nx,nrdata,nrwells)
+   real stddev(nx,nrdata,nrwells)
 
    type wellrates
       character(len=12) date
@@ -84,13 +83,14 @@ program sample
    close(10)
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! Read rates from schedule file
+! Extract rates from schedule file amd dump them in rates.txt
    print '(a,a)','string1=',trim(string1)
    status = system( string1 )
    print '(a,a)','string2=',trim(string2)
    status = system( string2 )
    status = system( 'head rates.txt' )
 
+! initializing rates to 0 and setting well names
    do i=1,nx
       do j=1,nrwells
          write(rates(i)%wellname(j),'(a,i1)')'OP_',j
@@ -101,6 +101,7 @@ program sample
       enddo
    enddo
 
+! reading rates and times from rates.txt
    open (10,file='rates.txt')
    idat=1
    do i=1,10000
@@ -124,6 +125,8 @@ program sample
       endif
    enddo
    200 close(10)
+
+! check file of all rate measurements
    open(10,file='output.dat')
       do i=1,nx
          do j=1,nrwells
@@ -133,46 +136,67 @@ program sample
       enddo
    close(10)
 
-  
-
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! scale with standard deviations
-
-
-
-   C=reshape(B,(/ nx,nrwells*nrdata, nrens /) )
-   do i=1,nx
-      do j=1,nrwells
-         obs(i,j,1) = rates(i)%opr(j)
-         obs(i,j,2) = rates(i)%wpr(j)
-         obs(i,j,3) = rates(i)%gpr(j)
+! Scaling perturbations according to measurement magnitude and error defs in obshistfile
+   do k=1,nx
+      do i=1,nrwells
+         obs(k,1,i) = rates(k)%opr(i)
+         obs(k,2,i) = rates(k)%wpr(i)
+         obs(k,3,i) = rates(k)%gpr(i)
       enddo
    enddo
-   obs1=reshape(obs,(/nx,nrwells*nrdata /) )
-
 
    open(10,file=trim(obshistfile),err=100)
-   do i=1,nrwells*nrdata
+   do m=1,100000
       read(10,'(tr21,a3,tr1,a4,tr11,f3.1,tr36,f6.1)',end=100) var,well,rel_err,min_err
       write(*,'(a,a3,a,a4,a,f3.1,a,f6.1)') ' var=',var,' well=',well,' rel_err=',rel_err,' min_err=',min_err
+      select case (var)
+      case('OPR')
+         l=1
+      case('WPR')
+         l=2
+      case('GPR')
+         l=3
+      end select
+      read(well(4:4),'(i1)')i
+
       do k=1,nx
-         if (obs1(k,i)/=0.0) then
-            stddev(k,i)=max(abs(rel_err*obs1(k,i)),min_err)
+         if (obs(k,l,i) /= 0.0) then
+            stddev(k,l,i)=max(abs(rel_err*obs(k,l,i)),min_err)
          else
-            stddev(k,i)=0.0
+            stddev(k,l,i)=0.0
          endif
       enddo
    enddo
    100 close(10)
 
-   do j=1,nrens
-      do i=1,nrwells*nrdata
-         do k=1,nx
-            C(k,i,j)=stddev(k,i)*C(k,i,j)
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Saving perturbations for schedule parcing
+   C=reshape(B,(/ nx, nrdata, nrwells, nrens /) )
+   open(10,file='Epert_0.dat')
+      do j=1,nrens
+         do i=1,nrwells
+            do l=1,nrdata
+               do k=1,nx
+                  C(k,l,i,j)=stddev(k,l,i)*C(k,l,i,j)
+                  write(10,'(4i4,e15.6)')k,l,i,j,C(k,l,i,j)
+               enddo
+            enddo
          enddo
       enddo
-   enddo
+   close(10)
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Saving perturbations for analysis routine
+   open(10,file='E.dat')
+      do j=1,nrens
+      do i=1,nx*nrwells*nrdata
+         write(10,'(i6,i4,e15.6)')i,j,A(i,j)
+      enddo
+      enddo
+   close(10)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ! testplot
    A=reshape(C,(/ nx*nrwells*nrdata, nrens /) )
    open(10,file='tec_ens1D_B.dat')
@@ -180,17 +204,6 @@ program sample
       write(10,'(i3,f12.2,100f12.2)')i,(i-1)*dx,A(i,1:min(nrens,100))
    enddo
    close(10)
-
-
-   open(10,file='E.dat')
-   do j=1,nrens
-   do i=1,nx*nrwells*nrdata
-      write(10,'(i6,i4,e15.6)')i,j,A(i,j)
-   enddo
-   enddo
-   close(10)
-
-
 
 
 end program
